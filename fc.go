35dd2b5bd8d600f1e9dc4b50694e5b0536fec31c
Dev Eloper <node@sloppy.pink>
2023-01-19 15:21:37 -0500

    form contract

--- fc.go
+++ fc.go
@@ -1,28 +1,32 @@
 package main
 
 import (
-		"crypto/ed25519"
-		"context"
+	"fmt"
+	"crypto/ed25519"
+	"context"
 
-    //"lukechampine.com/us/ed25519hash"
-    "lukechampine.com/us/renter"
-    "lukechampine.com/us/renter/proto"
-    "lukechampine.com/us/renter/renterutil"
-    //"lukechampine.com/us/renterhost"
+	//"lukechampine.com/us/ed25519"
+	"lukechampine.com/us/hostdb"
+  "lukechampine.com/us/renter"
+  "lukechampine.com/us/renter/proto"
+  "lukechampine.com/us/renter/renterutil"
+  //"lukechampine.com/us/renterhost"
 
-    "lukechampine.com/frand"
-    //"gitlab.com/NebulousLabs/Sia/crypto"
+  "lukechampine.com/frand"
+  //"gitlab.com/NebulousLabs/Sia/crypto"
 )
 
 func main() {
 
-const siadPassword = "c4c5f8884a5d6c4f731c24c75378b2b3" // found in ~/.sia/apipassword
-siad := renterutil.NewSiadClient(":9980", siadPassword)
+const siadPassword = "siaP4ss" // found in ~/.sia/apipassword
+siad := renterutil.NewSiadClient("localhost:9980", siadPassword)
 
 const hostPubKey = "ed25519:25407e439611ef4310034a07b6f618292117857bd7d9b2025db6e6da1102efde"
 
 hostIP, _ := siad.ResolveHostKey(hostPubKey)
 host, _ := hostdb.Scan(context.Background(), hostIP, hostPubKey)
+
+fmt.Printf("%+v\n", host)
 
 const uploadedBytes = 1e9
 const downloadedBytes = 2e9
@@ -34,13 +38,24 @@ totalFunds := uploadFunds.Add(downloadFunds).Add(storageFunds)
 
 currentHeight, _ := siad.ChainHeight()
 start, end := currentHeight, currentHeight+duration
+
 key := ed25519.NewKeyFromSeed(frand.Bytes(32))
-rev, _ := proto.FormContract(siad, siad, key, host, totalFunds, start, end)
+
+fmt.Println("Generated renter key.")
+
+rev, contractTxn, err := proto.FormContract(siad, siad, key, host, totalFunds, start, end)
+if err != nil {
+		fmt.Println(err)
+}
+
 contract := renter.Contract{
     HostKey:   rev.HostKey(),
     ID:        rev.ID(),
     RenterKey: key,
 }
+
+fmt.Println("Formed contract with ID ", contract.ID )
+fmt.Printf("%+v\n", contractTxn)
 
 //contractPath := "mycontract.contract"
 //_ = renter.SaveContract(contract, contractPath)
